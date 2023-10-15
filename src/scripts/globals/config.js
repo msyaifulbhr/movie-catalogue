03720b0dcc3df68b9f336e645c1968679e205ed9
msyaifulbhr <syaifulyy@gmail.com>
2023-10-15 12:26:22 +0700

    display content

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+  KEY: '61e128e196425342f6e52e7cb3f73a27',
+  BASE_URL: 'https://api.themoviedb.org/3/',
+  BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+  DEFAULT_LANGUAGE: 'en-us',
+};
+
+export default CONFIG;
